17fb1a9c6a1d89e5071d303fc38cc2a0d76246a6
qpjht196 <118255590+qpjht196@users.noreply.github.com>
2022-12-10 02:40:24 +0000

    formmated

--- gcd.c
+++ gcd.c
@@ -1,19 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-int a,b,r;
-// a = b*q + r
-// a 被除數 = b 除數 * q商 + r餘數
-// (a,b) = (b,r)
-scanf("%d",&a);
-scanf("%d",&b);
-while(b!=0)
-{
-//計算 r
-//前⾯變成b
-//後⾯變成r
-printf("a=%d b=%d r=%d\n",a,b,r);
-system("pause");
-}
-printf("最⼤公因數為%d",a);
+    int a, b, r;
+    // a = b*q + r
+    // a 被除數 = b 除數 * q商 + r餘數
+    // (a,b) = (b,r)
+    scanf("%d", &a);
+    scanf("%d", &b);
+    while (b != 0)
+    {
+        // 計算 r
+        // 前⾯變成b
+        // 後⾯變成r
+        printf("a=%d b=%d r=%d\n", a, b, r);
+        system("pause");
+    }
+    printf("最⼤公因數為%d", a);
 }
